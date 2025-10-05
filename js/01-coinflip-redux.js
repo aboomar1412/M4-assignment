ca6d392c4b3832703ce5d4eae12eea4a3cf9e441
Eias <aboomar5464@gmail.com>
2025-10-04 18:05:10 -0700

    01-coinflip-redux code

---
+++ js/01-coinflip-redux.js
@@ -0,0 +1,14 @@
+// Coin Flip Game Redux
+
+let coinFlip;
+let numFlips = parseInt(prompt("How many times would you like to flip the coin?"));
+
+for (let i = 0; i < numFlips; i++) {
+  coinFlip = Math.round(Math.random());
+  
+  if (coinFlip === 0) {
+    console.log("Heads");
+  } else {
+    console.log("Tails");
+  }
+}
