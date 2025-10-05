5ea7a14853835f959d03128d9ced27a4ba0e2628
Eias <aboomar5464@gmail.com>
2025-10-04 18:12:14 -0700

    02-coinflip-streak code

---
+++ js/02-coinflip-streak.js
@@ -0,0 +1,14 @@
+// Coin Flip Streak Game
+
+let coinFlip;
+
+do {
+  coinFlip = Math.round(Math.random());
+  
+  if (coinFlip === 0) {
+    console.log("Heads");
+  } else {
+    console.log("Tails");
+  }
+
+} while (coinFlip === 0); // keep looping while it's heads
